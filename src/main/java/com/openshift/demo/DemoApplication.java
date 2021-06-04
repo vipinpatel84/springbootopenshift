642fe15834fd372ffee8383a2a21d0c24a115723
Vipin Patel <73433956+vipinpatel84@users.noreply.github.com>
2021-06-04 13:27:57 +0530

    Updated Message

--- src/main/java/com/openshift/demo/DemoApplication.java
+++ src/main/java/com/openshift/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	
 	@GetMapping("/")
 	public String getHelloWorld() {
-		return "Welcome to Springboot Application Test CI/CD ";
+		return "Welcome to Springboot";
 	}
 
 }
