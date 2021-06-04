b340b2a3efd26254a75be1487255f0d13d3082c1
Vipin Patel <73433956+vipinpatel84@users.noreply.github.com>
2021-06-04 12:52:48 +0530

    Update DemoApplication.java
    Changes to Test Webhook

--- src/main/java/com/openshift/demo/DemoApplication.java
+++ src/main/java/com/openshift/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	
 	@GetMapping("/")
 	public String getHelloWorld() {
-		return "Welcome to Springboot Application Test CI/CD Openshift 1";
+		return "Welcome to Springboot Application Test CI/CD Openshift WebHook Access";
 	}
 
 }
