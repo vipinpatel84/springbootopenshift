2b1926df957a72baae77e52d7987c75b8d9659c3
Vipin Patel <73433956+vipinpatel84@users.noreply.github.com>
2021-06-15 22:31:48 +0530

    Update DemoApplication.java

--- src/main/java/com/openshift/demo/DemoApplication.java
+++ src/main/java/com/openshift/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 	
 	@GetMapping("/")
 	public String getHelloWorld() {
-		return "Welcome to Springboot jenkins OpenShift";
+		return "Welcome to Springboot jenkins OpenShift checking deployment";
 	}
 
 }
