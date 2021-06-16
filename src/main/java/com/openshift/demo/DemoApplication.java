01135ed4fc15689cf3adf0f4f0a0a5c60770479c
vipin <vipin@vipin-GL65-Leopard-9SCXK>
2021-06-17 00:14:15 +0530

    Spring boot Env added

--- src/main/java/com/openshift/demo/DemoApplication.java
+++ src/main/java/com/openshift/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.openshift.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	@Value("env_name")
+	private String env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
 	@GetMapping("/")
 	public String getHelloWorld() {
-		return "Welcome to Springboot jenkins OpenShift checking deployment version 1";
+		return "Welcome to Springboot jenkins OpenShift checking deployment version 1" + env;
 	}
 
 }
